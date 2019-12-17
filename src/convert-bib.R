3f215abb028f26111e752b4f5188740484b71d81
Steve Simon <mail@pmean.com>
2019-12-16 23:06:46 -0600

    Created convert-bib.R

--- src/convert-bib.R
+++ src/convert-bib.R
@@ -1,45 +1,33 @@
----
-title: "Add headers to md files"
-author: "Steve Simon"
-date: "6/28/2019"
-output: html_document
----
+#  convert-bib.R
+## written by Steve Simon
+## created 2019-12-16
 
-```{r setup, echo=FALSE}
-knitr::opts_chunk$set(echo=TRUE)
-knitr::opts_chunk$set(error=TRUE)
-source(file="prelims.R", echo=FALSE)
+source(file="src/prelims.R", echo=FALSE)
 verbose <- TRUE
-```
 
 ## Step 1. Find the .bib files
 
-```{r list-dirs}
-bib_names <- NULL
 r1_names <- NULL
-short_names <- NULL
-r1_root <- "c:/Users/steve/Dropbox/professional/web/r1/19"
-month_list <- c("05", "06", "07", "10", "11", "12")
-for (i_month in month_list[1:3]) {
+r1_dirs <- NULL
+r1_root <- "c:/Users/steve/Dropbox/professional/web/r1"
+month_list <- c("19/05", "19/06", "19/07", "19/10", "19/11", "19/12")
+for (i_month in month_list) {
   r1_path <- r1_root %s% i_month
   f0 <- list.files(path=r1_path, pattern="*.nbib")
   f1 <- list.files(path=r1_path, pattern="*.bib")
   fn <- setdiff(f1, f0)
   r1_names %<>% append(fn)
-  bib_names %<>% append(r1_path %s% fn)
-  short_names %<>% append("19" %s% i_month %s% r1_names)
+  r1_dirs %<>% append(rep(i_month, length(fn))) 
 }
-short_names <- sub(".bib", "", short_names)
-if (verbose) print(r1_names)
+short_names <- r1_dirs %s% sub(".bib", "", r1_names)
+bib_names <- r1_root %s% r1_dirs %s% r1_names
+if (verbose) print(short_names)
 n_names <- length(r1_names)
 r3_path <- "c:/Users/steve/Dropbox/professional/web/r3"
-```
 
 ## Step 2. Read each .bib file and convert to .md file
+### First, set up the general structure.
 
-First, set up the general structure.
-
-```{r structure}
 yaml_divider <- '---'
 marks <- c(
   "title = ",
@@ -60,30 +48,25 @@ default_marks <- c(
   "No short name"
 )
 n_marks <- length(marks)
-```
 
-Create bib_info, a matrix of information about the individual .bib files.
+### Create bib_info, a matrix of information about the individual .bib files.
 
-```{r create-info}
 bib_cats <- c(
   "fil",
+  "nam",
   "ttl",
   "dat",
   "tag",
   "not",
   "url",
   "pdf",
-  "nam",
   "png"
 )
 n_cats <- length(bib_cats)
 bib_info <- matrix("<<empty>>", nrow=n_names, ncol=n_cats)
 dimnames(bib_info)[[2]] <- bib_cats
-```
 
-Populate bib_info
-
-```{r bib-functions}
+### Populate bib_info
 
 read_bib <- function(fn) {readLines(r1_path %s% r1_names[i_file])}
 
@@ -99,7 +82,8 @@ select_txt <- function(txt, lab, def) {
     return(def)
   }
   lin %>%
-    remove_ch('^@.*\\{'   ) %>%          # from @ to {
+    remove_ch('^.*@'      ) %>%          # from beginning to @
+    remove_ch('^.*\\{'    ) %>%          # from beginning to {
     remove_ch(',.*?$'     ) %>%          # from comma to end
     remove_ch('\\"'       ) %>%          # quote marks
     remove_ch('^\\s*'     ) %>%          # leading spaces
@@ -112,10 +96,6 @@ select_txt <- function(txt, lab, def) {
   return(paste0(selection, collapse="; "))
 }
 
-```
-
-
-```{r read-bib}
 txt_original <- as.list(1:n_names)
 for (i_file in 1:n_names) {
   if (verbose) {cat("\n", short_names[i_file], sep="")}
@@ -131,12 +111,10 @@ for (i_file in 1:n_names) {
   bib_info[i_file, "pdf"] <- select_txt(tx, "pdf="          , "No pdf")
   bib_info[i_file, "nam"] <- select_txt(tx, "@"             , "No name")
   
-  bib_info[i_file, "png"] <- r1_path %s% sub("bib$", "png", r1_names[i_file])
+  bib_info[i_file, "png"] <- sub("bib$", "png", bib_names[i_file])
 
 }
-```
 
-```{r build-md}
 for (i_file in 1:n_names) {
   yaml_divider                                               %1%
     'title: '                    %q% bib_info[i_file, "ttl"] %1%
@@ -145,6 +123,7 @@ for (i_file in 1:n_names) {
     'category: '                 %0% 'Recommended'           %1%
     'tags: '                     %q% bib_info[i_file, "tag"] %1%
     'output: '                   %0% 'html_document'         %1%
+    'source: '                   %0% bib_info[i_file, "fil"] %1%
     yaml_divider                                             %2%
 
     bib_info[i_file, "not"]                                  %2%
@@ -161,22 +140,19 @@ for (i_file in 1:n_names) {
     sub(" or [PDF format](No pdf)",  "", ., fixed=TRUE)  %>%
     sub(" [html format](No html) or", "", ., fixed=TRUE) -> md_file
   new_name <- r3_path %s% bib_info[i_file, "nam"] %0% ".md"
-  if (verbose) {"Writing" %b% cat(new_name %b% ".\n")}
+  if (verbose) {
+    "\nStep" %b%
+    i_file %b%
+    ": writing" %b% 
+    bib_info[i_file, "fil"] %b%
+    "to" %b%
+    bib_info[i_file, "nam"] %0%
+    "." %>% cat
+  }
   writeLines(md_file, new_name)
 }
-```
 
 ## Step 3. Produce an index
-
-```{r produce-index, eval=FALSE} 
-o <- rev(order(bib_dates))
-bib_dates <- bib_dates[o]
-bib_names <- bib_names[o]
-txt_full <- as.list(1:bib_count)
-for (i in 1:bib_count) {
-  txt_full[[i]] <- txt_original[[o[i]]]
-}
-```
 
 ## Step 4. Add a footer
 
@@ -184,7 +160,6 @@ for (i in 1:bib_count) {
 
 ## Step 6. List incomplete areas
 
-```{r incomplete}
 bib_df <- data.frame(bib_info, stringsAsFactors=FALSE)
 bib_df %>%
   filter(tag=="No tags") %>%
@@ -196,11 +171,7 @@ bib_df %>%
   mutate(png=substr(png, 47, 99)) %>%
   mutate(png=sub(".png", "", png)) %>%
   select(tag, png)
-```
 
+## Save everything
 
-Save everything.
-
-```{r save-everything}
-save.image("../data/create-r3.RData")
-```
+save.image("data/create-r3.RData")
