d4c9e80c420d21993513cb9f04efc7efaf84afbb
Steve Simon <mail@pmean.com>
2020-01-17 06:54:37 -0600

    Work on renaming bib files.

--- src/step0-rename-bib-files.R
+++ src/step0-rename-bib-files.R
@@ -11,10 +11,22 @@ if (!exists("verbose")) verbose <- TRUE
 if (!exists("update_all")) update_all <- TRUE
 
 file_list <- build_file_list("../source/bib", "*.bib")
+field_header <- "name"
 for (i_file in file_list) {
   tx <- readLines(i_file)
   fields <- parse_bibtex(tx, i_file)
-  if(verbose) i_file %C% fields$name %>% br %>% cat
+  tx[1] %>%
+    str_remove("^.*@") %>%
+    str_remove("\\{.*") -> left_side
+  i_file %>%
+    str_remove("^.*/") %>%
+    str_remove(fixed(".bib")) -> right_side
+  tx[1] <- "@" %0% left_side %0% "{" %0% right_side
+  i_file %>%
+    str_replace("/source/bib/", "/update/") -> new_file
+  # if(verbose) i_file %C% fields[[field_header]] %>% br %>% cat
+  # if(verbose) i_file %C% tx[1] %>% br %>% cat
+  if(verbose) i_file %C% new_file %>% br %>% cat
   if(is.null(fields[[field_header]])) print(fields)
 }
 
